2663c6cbdd0a833438544e18d8bba4a4474d5c0a
ThanhNhanDang <64689691+ThanhNhanDang@users.noreply.github.com>
2022-05-17 17:09:44 +0700

    finish

---
+++ frontend/src/components/Feed.jsx
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+
+import { client } from "../client";
+import { searchQuery, feedQuery } from "../utils/data";
+import MasonryLayout from "./MasonryLayout";
+import Spinner from "./Spinner";
+
+const Feed = () => {
+  const [pins, setPins] = useState();
+  const [loading, setLoading] = useState(false);
+  const { categoryId } = useParams();
+
+  useEffect(() => {
+    if (categoryId) {
+      setLoading(true);
+      const query = searchQuery(categoryId);
+      client.fetch(query).then((data) => {
+        setPins(data);
+        setLoading(false);
+      });
+    } else {
+      setLoading(true);
+
+      client.fetch(feedQuery).then((data) => {
+        setPins(data);
+        setLoading(false);
+      });
+    }
+  }, [categoryId]);
+  const ideaName = categoryId || "new";
+
+  if (loading)
+    return (
+      <Spinner message={`We are adding ${ideaName} ideas to your feed!`} />
+    );
+
+  if(!pins?.length) return<h1 className="mt-10 text-center text-xl">No pin available</h1>
+  return <div> {pins && <MasonryLayout pins={pins} />}</div>;
+};
+
+export default Feed;
